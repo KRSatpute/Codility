9c43b35f04424bb5edd7a8f7764549e1bda72498
Kshitij Satpute <kshitij.s@visiblealpha.com>
2020-10-08 22:26:36 -0300

    disk 68 pct sol

--- LessonSix.cs
+++ LessonSix.cs
@@ -6,6 +6,42 @@ namespace Codility
 {
     public static class LessonSix
     {
+        public static int NumberOfDiscIntersections(int [] A)
+        {
+            if (A.Length == 0)
+                return 0;
+            
+            int distinctIntersections = 0;
+
+            for (int i = 0; i < A.Length - 1; i++)
+            {
+                for (int j = i + 1; j < A.Length; j++)
+                {
+                    int sumOfRadius;
+                    int disctanceBetweenCenters = j - i;
+                    try
+                    {
+                        checked
+                        {
+                            sumOfRadius = A[i] + A[j];
+                        }
+                    }
+                    catch(OverflowException)
+                    {
+                        sumOfRadius = int.MaxValue;
+                    }
+
+                    if (disctanceBetweenCenters <= sumOfRadius)
+                        distinctIntersections += 1;
+                    
+                    if (distinctIntersections > 1000000000)
+                        return -1;
+                }
+            }
+
+            return distinctIntersections;
+        }
+
         public static int Distinct(int[] A)
         {
             if (A.Length == 0)
